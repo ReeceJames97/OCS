7cd592f2287b71c3e2734329c65f32994daebe9f
ReeceJames97 <hein1234.kg111@gmail.com>
2023-09-26 16:40:26 +0630

    updated UI for Home page

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color toastBackgroundColor = Color(0xFF212121);
+  static const Color standardBtnColor = Color(0xFF252B48);
+  static const Color snackBarBgColor = Color(0xFF212121);
+  static const Color btnDisableColor = Color(0xFFaaaaaa);
+  static const Color primaryColor = Color(0xFF363C97);
+  static const Color secondaryColor = Color(0xFF4E59A8);
+  static const Color backgroundColor = Color(0xFFEEEEEE);
+  static const Color appBarColor = Color(0xFF1C2136);
+}
